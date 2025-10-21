230c83c4a6f6c4b773d4161c4a4055c7df267a93
Alex <hanksalexander1@gmail.com>
2025-10-21 09:02:44 -0400

    actually done

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,34 +1,28 @@
 import random
 
 
-def garbage():
-    if 0 > ask > 8:
-        while True:
-            ask = int(input("enter 0-8 -> "))
-            if 8 >= ask >= 0:
-                break 
-        turns += 1
-        while turns < 9:
-            turns = 0
-            tic = initialize_board()
-            print_board(tic)
-            pass
-
-#board = [0,1,2,3,4,5,6,7,8]
-
 def initialize_board():
+    """
+    initializes the board
+    """
     
     tic = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
     return tic
 
 def print_board(tic):
+    """
+    prints the board
+    """
     print(f"{tic[0]} | {tic[1]} | {tic[2]} ")
     print(f"{tic[3]} | {tic[4]} | {tic[5]} ")
     print(f"{tic[6]} | {tic[7]} | {tic[8]} ")
         
 
 def make_move(tic):
+    """
+    has the user enter where they want to go and checks if it is possible
+    """
     while True:
         try:
             ask = int(input("Enter a position (0-8): "))
@@ -54,10 +48,16 @@ def replace_number(tic, ask):
     return False
 
 def check_draw(tic):
+    """
+    checks if the board is full
+    """
     tic == initialize_board
     return all(isinstance(spot, str) for spot in tic)
 
 def computer_move(tic):
+    """
+    gives the computer a list of possible moves from the non selected intergers
+    """
     available_moves = [i for i in range(9) if isinstance(tic[i], int)]
     computer_guess = random.choice(available_moves)
     tic[computer_guess] = "O"
@@ -66,6 +66,9 @@ def computer_move(tic):
 
 
 def check_winner(tic):
+    """
+    checks weather the user or the computer won
+    """
     win_combos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],  
         [0, 3, 6], [1, 4, 7], [2, 5, 8],  
@@ -79,6 +82,9 @@ def check_winner(tic):
     return None
 
 def retry():
+    """
+    asks if the user wants to retry and if so restards the code over again
+    """
     retrying = input("if you want to play again enter either yes or no: ->").strip().lower()
     while retrying != "no" and retrying != "yes":
         print("you did not enter yes or no")
@@ -90,6 +96,9 @@ def retry():
     
 
 def play_game():
+    """
+    main code that ties everything together
+    """
     tic = initialize_board()
     while True:
         print_board(tic)
